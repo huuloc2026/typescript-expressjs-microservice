dd64d1d585529e43cd2e88d35afb98bcac549d0f
HuuLoc <huuloc@192.168.x.x.non-exists.ptr.local>
2025-02-11 21:42:48 +0700

    update package

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 
 dotenv.config();
-
+console.clear();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
